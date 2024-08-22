b4e3edd59b4a5afda1f44b325d030a9bc9b8974f
MuhRandy <muhammadrandy0027@gmail.com>
2024-08-22 09:50:11 +0800

    feat: add basic UI

--- script.js
+++ script.js
@@ -2,13 +2,6 @@ function Gameboard() {
   const board = [];
   const size = 3;
 
-  for (let i = 0; i < size; i++) {
-    board[i] = [];
-    for (let j = 0; j < size; j++) {
-      board[i].push(Cell());
-    }
-  }
-
   const getBoard = () => board;
 
   const getBoardSize = () => size;
@@ -29,7 +22,18 @@ function Gameboard() {
     board[row][column].addMark(playerMark);
   };
 
-  return { getBoard, getBoardSize, printBoard, markCell };
+  const resetBoard = () => {
+    for (let i = 0; i < size; i++) {
+      board[i] = [];
+      for (let j = 0; j < size; j++) {
+        board[i].push(Cell());
+      }
+    }
+  };
+
+  resetBoard();
+
+  return { getBoard, getBoardSize, printBoard, resetBoard, markCell };
 }
 
 function Cell() {
@@ -55,7 +59,9 @@ function GameController(playerOne = "Player One", playerTwo = "Player Two") {
     return { name, mark, getScore, addScore };
   };
 
-  const players = [Player(playerOne, "x"), Player(playerTwo, "o")];
+  const players = [Player(playerOne, "X"), Player(playerTwo, "O")];
+
+  const getPlayers = () => players;
 
   let activePlayer = players[0];
 
@@ -72,8 +78,10 @@ function GameController(playerOne = "Player One", playerTwo = "Player Two") {
     console.log(`${getActivePlayer().name}'s turn.`);
   };
 
-  const isPlayerWin = (playerMark, boardSize) => {
+  const isPlayerWin = () => {
     let activePlayerMarkNumber;
+    const playerMark = getActivePlayer().mark;
+    const boardSize = board.getBoardSize();
 
     const firstDiagonal = [];
     const secondDiagonal = [];
@@ -121,7 +129,7 @@ function GameController(playerOne = "Player One", playerTwo = "Player Two") {
       if (activePlayerMarkNumber === boardSize) return true;
     };
 
-    for (let i = 0; i < board.getBoardSize(); i++) {
+    for (let i = 0; i < boardSize; i++) {
       if (isPlayerWinOnDiagonal(i)) return true;
 
       if (isPlayerWinOnRow(i)) return true;
@@ -156,7 +164,7 @@ function GameController(playerOne = "Player One", playerTwo = "Player Two") {
 
     if (isGameDraw()) return console.log("Game draw");
 
-    if (isPlayerWin(getActivePlayer().mark, board.getBoardSize())) {
+    if (isPlayerWin()) {
       getActivePlayer().addScore();
       return console.log(`${getActivePlayer().name} win`);
     }
@@ -167,18 +175,102 @@ function GameController(playerOne = "Player One", playerTwo = "Player Two") {
 
   printNewRound();
 
-  return { playRound, getActivePlayer };
+  return {
+    playRound,
+    getActivePlayer,
+    getPlayers,
+    getBoard: board.getBoard,
+    resetBoard: board.resetBoard,
+    isPlayerWin,
+    isGameDraw,
+  };
 }
 
-const game = GameController();
+function ScreenController() {
+  const game = GameController();
+  const playerTurnHeading = document.querySelector(".player-turn");
+  const boardDiv = document.querySelector(".board");
+  const dialog = document.querySelector("dialog");
 
-// // draw scenario
-// game.playRound(0, 0);
-// game.playRound(0, 1);
-// game.playRound(1, 1);
-// game.playRound(1, 0);
-// game.playRound(2, 1);
-// game.playRound(0, 2);
-// game.playRound(1, 2);
-// game.playRound(2, 0);
-// game.playRound(2, 2);
+  const updateScreen = () => {
+    // get newest players score
+    const players = game.getPlayers();
+    const playerOneName = document.querySelector(".player-one-name");
+    const playerTwoName = document.querySelector(".player-two-name");
+    const playerOneMark = document.querySelector(".player-one-mark");
+    const playerTwoMark = document.querySelector(".player-two-mark");
+
+    playerOneName.textContent = players[0].name;
+
+    playerOneMark.textContent = `${players[0].mark}: ${players[0].getScore()}`;
+
+    playerTwoName.textContent = players[1].name;
+
+    playerTwoMark.textContent = `${players[1].mark}: ${players[1].getScore()}`;
+
+    // clear the board
+    boardDiv.textContent = "";
+
+    // get the newest version of the board and player turn
+    const board = game.getBoard();
+    const activePlayer = game.getActivePlayer();
+
+    // Display player's turn
+    playerTurnHeading.textContent = `${activePlayer.name}'s turn...`;
+
+    // Render board squares
+    board.forEach((row, rowIndex) => {
+      row.forEach((cell, columnIndex) => {
+        // Anything clickable should be a button!!
+        const cellButton = document.createElement("button");
+        cellButton.classList.add("cell");
+        // Create a data attribute to identify the column
+        // This makes it easier to pass into our `playRound` function
+        cellButton.dataset.row = rowIndex;
+        cellButton.dataset.column = columnIndex;
+        cellButton.textContent = cell.getValue();
+        boardDiv.appendChild(cellButton);
+      });
+    });
+  };
+
+  // Add event listener for the board
+  function clickHandlerBoard(e) {
+    const selectedRow = e.target.dataset.row;
+    const selectedColumn = e.target.dataset.column;
+    // Make sure I've clicked a column and not the gaps in between
+    if (!selectedColumn || !selectedRow) return;
+
+    game.playRound(selectedRow, selectedColumn);
+
+    // check is the game over
+    if (game.isPlayerWin() || game.isGameDraw()) {
+      const dialogHeading = document.querySelector(".game-status h1");
+      const dialogDiv = document.querySelector(".game-status div");
+
+      dialogHeading.textContent = game.isPlayerWin()
+        ? `${game.getActivePlayer().name}`
+        : "Game";
+
+      dialogDiv.textContent = game.isPlayerWin() ? "Win" : "Draw";
+
+      dialog.show();
+    }
+    updateScreen();
+  }
+
+  function clickHandlerDialog() {
+    game.resetBoard();
+    updateScreen();
+    dialog.close();
+  }
+  boardDiv.addEventListener("click", clickHandlerBoard);
+  dialog.addEventListener("click", clickHandlerDialog);
+
+  // Initial render
+  updateScreen();
+
+  // We don't need to return anything from this module because everything is encapsulated inside this screen controller.
+}
+
+ScreenController();
